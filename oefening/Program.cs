381eb58688e04e95cd6fd1087e5bbb4d0fbd0327
tinahauq <40709994+tinahauq@users.noreply.github.com>
2018-08-23 00:31:30 +0200

    Added exercises

---
+++ oefening/Program.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Linq;
+
+namespace First
+{
+    class Program
+    {
+
+        //Algemene opmerking: kijk bij het uitwerken van deze oefeningen steeds na of de System.String class geen method
+        //heeft die gebruikt kan worden bij het oplossen van de oefening!
+
+
+        /* Schrijf een functie die twee opgegeven strings vergelijkt. Indien de strings identiek zijn (ook wat hoofd/kleine
+         * letters betreft), dan geef je 'Identiek' als functieresultaat. Zijn de strings verschillend dan is het functieresultaat 
+         * 'Verschillend'. Zijn de strings gelijk, maar verschillen ze op gebied van hoofdletters of kleine letters, dan 
+         * geef je 'Verschil letters' als resultaat . !!!!String.Compare() !!!!
+        */
+
+        public static string VergelijkStrings(string s1, string s2)
+        {
+            // schrijf hier je code
+            return "";
+        }
+
+        /* Schrijf een functie die nagaat of een opgegeven woord in een array van strings voorkomt. Als resultaat geef je
+         * true (het woord komt in de array voor) of false (het woord komt niet in de array voor).
+        */
+
+        public static bool ZoekWoord(string woord, string zin)
+        {
+            // schrijf hier je code
+            return true;
+        }
+
+        /* Schrijf een functie die telt hoeveel keer een opgegeven letter voorkomt in een opgegeven zin.
+        */
+
+        static public int TelLetter(char letter, string zin)
+        {
+            // schrijf hier je code
+            return 0;
+        }
+
+
+        /* Schrijf een functie die de klinkers in een opgegeven zin vervangt door een * en de aangepaste zin als 
+         * functieresultaat geeft. !!!!String.Replace()!!!!
+         */
+
+        public static string VeranderKlinkers(string zin)
+        {
+            // schrijf hier je code
+            return "";
+        }
+
+        /* Schrijf een functie die nagaat of een ingegeven woord een palindroom is. Het resultaat van de functie is true als
+         * het woord een palindroom is, false als het geen palindroom is. !!!!StrReverse() opzoeken!!!!
+        */
+        public static bool IsPalindroom(string woord)
+        {
+            // schrijf hier je code
+            return true;
+        }
+
+        /* Schrijf een functie die telt hoeveel karakters een string bevat, nadat de overtollige spaties in het begin en
+         * het einde verwijderd zijn. Geef het aantal karakters als functieresultaat. !!!!Trim()!!!!
+        */
+        public static int TelKarakters(string zin)
+        {
+            // schrijf hier je code
+            return 0;
+        }
+
+        /* Schrijf een functie die het langste woord in een opgegeven zin zoekt en dit woord als functieresultaat geeft.
+         * Indien er meerdere woorden dezelfde lengte hebben, geef je één van deze woorden als resultaat.
+        */
+        public static string ZoekLangsteWoord(string zin)
+        {
+            // schrijf hier je code
+            return "";
+        }
+
+        /* Schrijf een functie die zoekt welke klinker het meest voorkomt in een string en deze klinker als 
+         * functieresultaat geeft. Hoofdletters en kleine letters van dezelfde klinker mogen samengeteld worden.
+         * Maak bij deze oefening gebruik van een array voor het opslaan van de klinker-tellers en van een array die 
+         * de klinkers bevat.
+         * Zoek op hoe je de functies Max() en IndexOf() kan gebruiken in combinatie met deze arrays om de code op een
+         * efficiënte manier uit te werken.
+         * Indien meerdere klinkers hetzelfde aantal keren voorkomen in de string, geef je één van deze klinkers als resultaat.
+        */
+
+        public static char ZoekKlinker(string zin)
+        {
+            // schrijf hier je code
+            return ' ';
+        }
+
+        /* Ontwerp een consoletoepassing waarmee je een Belgisch ondernemingsnummer kan controleren. 
+         * Een Belgisch ondernemingsnummer is als volgt opgebouwd: BE 0xxx.xxx.xxx waarbij BE de landcode is die standaard gevolgd
+         * wordt door een spatie en dan een 0. Wanneer je de volgende 7 cijfers deelt door 97 in een gehele deling, dan is het 
+         * getal gevormd door de laatste 2 cijfers gelijk aan 97 minus de rest van deze gehele deling. 
+         * Een ongeldig ondernemingsnummer genereert 'true' als functieresultaat, een ongeldig 'false'.
+        */
+        public static bool ControleerOndNr(string ondNr)
+        {
+            // schrijf hier je code
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Naam oefening \t\t Verwacht resultaat \t\t Jouw resultaat");
+            Console.WriteLine("VergelijkStrings \t Verschillend \t\t\t " + VergelijkStrings("Water", "Wind"));
+            Console.WriteLine("VergelijkStrings \t Identiek \t\t\t " + VergelijkStrings("Water", "Water"));
+            Console.WriteLine("VergelijkStrings \t Verschil letters \t\t " + VergelijkStrings("Water", "water"));
+            Console.WriteLine("ZoekWoord \t\t True \t\t\t\t " + ZoekWoord("processor", "geheugen toetsenbord processor scherm"));
+            Console.WriteLine("ZoekWoord \t\t False \t\t\t\t " + ZoekWoord("processor", "geheugen toetsenbord voeding scherm"));
+            Console.WriteLine("TelLetter \t\t 4 \t\t\t\t " + TelLetter('a', "Computers zijn handige apparaten."));
+            Console.WriteLine("TelLetter \t\t 0 \t\t\t\t " + TelLetter('u', "Deze zin bevat de letter niet."));
+            Console.WriteLine("VeranderKlinkers \t D*z* z*n b*v*t g**n kl*nk*rs. \t " + VeranderKlinkers("Deze zin bevat geen klinkers."));
+            Console.WriteLine("IsPalindroom \t\t False \t\t\t\t " + IsPalindroom("water"));
+            Console.WriteLine("IsPalindroom \t\t True \t\t\t\t " + IsPalindroom("lepel"));
+            Console.WriteLine("TelKarakters \t\t 18 \t\t\t\t " + TelKarakters("   Dit is de testzin.    "));
+            Console.WriteLine("ZoekLangsteWoord \t aangekomen \t\t\t " + ZoekLangsteWoord("De nieuwe computers zijn aangekomen op school."));
+            Console.WriteLine("ZoekKlinker \t\t i \t\t\t\t " + ZoekKlinker("Dit is een zin."));
+            Console.WriteLine("ZoekKlinker \t\t e \t\t\t\t " + ZoekKlinker("De naam van de straat is Esseneweg."));
+            Console.WriteLine("ControleerOndNr \t True \t\t\t\t " + ControleerOndNr("BE 0425.989.356"));
+            Console.WriteLine("ControleerOndNr \t False \t\t\t\t " + ControleerOndNr("BE 0425.989.355"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ oefening/Test.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using NUnitLite;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        // deze functie is nodig om de testen achteraf via de webinterface uit te voeren
+        public static int Run(string resultPath)
+        {
+            string[] args = { "--work=" + resultPath };
+            return new AutoRun().Execute(args);
+        }
+
+        [Test]
+        public void TestVergelijkStrings()
+        {
+            Assert.That(First.Program.VergelijkStrings("een", "twee"), Is.EqualTo("Verschillend"));
+            Assert.That(First.Program.VergelijkStrings("een", "een"), Is.EqualTo("Identiek"));
+            Assert.That(First.Program.VergelijkStrings("een", "Een"), Is.EqualTo("Verschil letters"));
+
+        }
+
+        [Test]
+        public void TestZoekWoord()
+        {
+            Assert.That(First.Program.ZoekWoord("test", "Het woord test komt in deze zin."), Is.EqualTo(true));
+            Assert.That(First.Program.ZoekWoord("test", "Het woord komt niet in de zin voor."), Is.EqualTo(false));
+
+        }
+
+        [Test]
+        public void TestTelLetter()
+        {
+            Assert.That(First.Program.TelLetter('o', "Hoe hoog is de berg?"), Is.EqualTo(3));
+            Assert.That(First.Program.TelLetter('o', "De weg is lang."), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestVeranderKlinkers()
+        {
+            Assert.That(First.Program.VeranderKlinkers("Dit is een testje."), Is.EqualTo("D*t *s **n t*stj*."));
+
+        }
+
+        [Test]
+        public void TestIsPalindroom()
+        {
+            Assert.That(First.Program.IsPalindroom("computer"), Is.EqualTo(false));
+            Assert.That(First.Program.IsPalindroom("koortsmeetsysteemstrook"), Is.EqualTo(true));
+
+        }
+
+        [Test]
+        public void TestTelKarakters()
+        {
+            Assert.That(First.Program.TelKarakters("     Een klavier is een invoerapparaat.  "), Is.EqualTo(34));
+
+        }
+
+        [Test]
+        public void TestZoekLangsteWoord()
+        {
+            Assert.That(First.Program.ZoekLangsteWoord("Het woord toetsenbord is een lang woord."), Is.EqualTo("toetsenbord"));
+
+        }
+
+        [Test]
+        public void TestZoekKlinker()
+        {
+            Assert.That(First.Program.ZoekKlinker("De Evenaar loopt om de aarde."), Is.EqualTo('e'));
+            Assert.That(First.Program.ZoekKlinker("We kopen oliebollen op school."), Is.EqualTo('o'));
+
+        }
+
+        [Test]
+        public void TestControleerOndNr()
+        {
+            Assert.That(First.Program.ControleerOndNr("BE 0461.324.971"), Is.EqualTo(true));
+            Assert.That(First.Program.ControleerOndNr("BE 0461.324.970"), Is.EqualTo(false));
+
+        }
+    }
+}
